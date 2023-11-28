c11e29d26ac6779cdc3d3a558ca1403544e91e1d
kimwoojin0081 <118324628+kimwoojin0081@users.noreply.github.com>
2023-11-28 15:32:34 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -25,8 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (username === "admin" && password === "admin") {
       alert("로그인 성공!");
-      window.location.href = "/LicenseLab/index.html";
-      //window.location.href = "/LicenseLab/mainsub.html";
+      window.location.href = "/LicenseLab/mainsub.html";
       signInLink.innerText = 'Sign out';
       isLoggedIn = !isLoggedIn;
       updateMenu();
