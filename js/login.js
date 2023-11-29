389c7d42f48fe1de0da45e7c88cf79c065861d8f
kimwoojin0081 <118324628+kimwoojin0081@users.noreply.github.com>
2023-11-29 13:47:14 +0900

    11-29 login.js V2

--- js/login.js
+++ js/login.js
@@ -2,23 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   
   let loginForm = document.getElementById("login-form-submit");
   let loginErrorMsg = document.getElementById("login-error-msg");
-  let lg = document.querySelector("#lgin");
 
   loginForm.addEventListener("click", (e) =>  {
     e.preventDefault();
-
     let username = document.getElementById("username-field").value;
     let password = document.getElementById("password-field").value;
 
     if (username === "admin" && password === "admin") {
       alert("로그인 성공!");
-      //--------------------------------------------------------------------
-      //임시 수정
-      //window.location.href = "/LicenseLab/index.html";
-     lg.textContent = "sign out";
-      //----------------------------------------------------------------------
-      // window.location.href = "/LicenseLab/mainsub.html";
-      updateMenu();
+      window.location.href = "/LicenseLab/mainsub.html";
     } else {
       alert("아이디 혹은 비밀번호를 잘못 입력하였습니다.");
       loginErrorMsg.style.opacity = 1;
